70169b0852adfb25ef6bbd087f07fa8f3f4ebc47
Laviel Dela Cruz <fafansin@gmail.com>
2025-02-18 01:29:56 +0300

    feat: filtering the products based on missing dimensions and weight

--- Block/Adminhtml/Settings/Index.php
+++ Block/Adminhtml/Settings/Index.php
@@ -3,20 +3,30 @@ namespace NBOX\Shipping\Block\Adminhtml\Settings;
 
 use Magento\Backend\Block\Template;
 use Magento\Backend\Block\Template\Context;
+//
 use NBOX\Shipping\Helper\ConfigHelper;
+use NBOX\Shipping\Helper\ProductHelper;
 
 class Index extends Template
 {
     protected $configHelper;
+    protected $productHelper;
 
     public function __construct(
         Context $context, 
         ConfigHelper $configHelper,
+        ProductHelper $productHelper,
         array $data = [], 
     ){
         $data['isLoggedIn'] = $configHelper->getApiToken() ? true : false;
         parent::__construct($context, $data);
         $this->configHelper = $configHelper; 
+        $this->productHelper = $productHelper; 
         //
     }
+
+    public function getProducts(){
+        return $this->productHelper->getProducts();
+    }
+
 }
---
+++ Helper/ProductHelper.php
@@ -0,0 +1,88 @@
+<?php
+namespace NBOX\Shipping\Helper;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Backend\Model\UrlInterface;
+
+class ProductHelper extends AbstractHelper
+{
+   protected $productRepository;
+   protected $searchCriteriaBuilder;
+   protected $filterBuilder;
+   protected $backendUrl;
+
+   public function __construct(
+      Context $context,
+      ProductRepositoryInterface $productRepository,
+      SearchCriteriaBuilder $searchCriteriaBuilder,
+      FilterBuilder $filterBuilder,
+      UrlInterface $backendUrl,
+
+   ) {
+      parent::__construct($context);
+      $this->productRepository = $productRepository;
+      $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+      $this->filterBuilder = $filterBuilder;
+      $this->backendUrl = $backendUrl;
+   }
+
+   /**
+    * Get products that need attention (active, non-virtual, with null or 0 weight, length, width, or height)
+    * 
+    * @return array
+    * @throws LocalizedException
+    */
+    public function getProducts()
+   {
+      // Create filters for active products and non-virtual products
+      $statusFilter = $this->filterBuilder
+         ->setField('status')
+         ->setValue(1)
+         ->setConditionType('eq')
+         ->create();
+   
+      $typeFilter = $this->filterBuilder
+         ->setField('type_id')
+         ->setValue('virtual')
+         ->setConditionType('neq')
+         ->create();
+   
+      // Create OR conditions for weight, length, width, and height being NULL or 0
+      $attributes = ['weight', 'length', 'width', 'height'];
+      $orFilters = [];
+   
+      foreach ($attributes as $attribute) {
+         // Condition for NULL
+         $orFilters[] = $this->filterBuilder
+               ->setField($attribute)
+               ->setConditionType('null')
+               ->create();
+   
+         // Condition for 0
+         $orFilters[] = $this->filterBuilder
+               ->setField($attribute)
+               ->setConditionType('eq')
+               ->setValue(0)
+               ->create();
+      }
+   
+      // Build search criteria
+      $this->searchCriteriaBuilder->addFilters([$statusFilter, $typeFilter]);
+   
+      // Apply OR condition for attributes (grouped under one OR condition)
+      $this->searchCriteriaBuilder->addFilters($orFilters, true); // 'true' makes it an OR condition
+   
+      $searchCriteria = $this->searchCriteriaBuilder->create();
+   
+      // Fetch products
+      $products = $this->productRepository->getList($searchCriteria)->getItems();
+   
+      return $products;
+   }
+   
+}
